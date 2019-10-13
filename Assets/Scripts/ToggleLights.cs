8dcf8119e4d30bc9e1607d03b87f19fe41875646
Jake TerHark <jterha2@uic.edu>
2019-10-12 20:43:54 -0500

    alpha

---
+++ Assets/Scripts/ToggleLights.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLights : MonoBehaviour
+{
+    public static bool toggle = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var l = this.gameObject.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void DoSomething() {
+        Debug.Log("triggered!");
+        var light = GameObject.Find("deskLight");
+        if (light.activeSelf)
+        {
+            light.SetActive(false);
+        }
+        else
+        {
+            light.SetActive(true);
+        }
+    }
+}
