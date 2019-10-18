b108e77138277eedec84368cd2bc7389b0a859f1
jaket <jterha2@uic.edu>
2019-10-17 23:54:08 -0500

    pencils

---
+++ Assets/Scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour
+{
+
+    private AudioSource src;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        src = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log("EVENT: Collision");
+        src.Play();
+    }
+
+    void OnCollisionExit(Collision collision) {
+        Debug.Log("EVENT: Collision Exit");
+        src.Stop();
+    }
+
+    public static void DoSomething(string param)
+    {
+    }
+}
