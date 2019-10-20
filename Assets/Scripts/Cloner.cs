f291edc8d9ef7cd610a316737b1b6f95899269af
jaket <jterha2@uic.edu>
2019-10-19 19:18:59 -0500

    backup

---
+++ Assets/Scripts/Cloner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloner : MonoBehaviour
+{
+    public GameObject cloneObject;
+    public GameObject location;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DoClone()
+    {
+        Debug.Log("Cloning...");
+        var obj = Object.Instantiate(cloneObject, location.transform.position, Quaternion.Euler(0, 0, 0));
+        obj.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float xSpeed, ySpeed, zSpeed;
+
+    private float deltaTime;
+    private float x, y, z;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime = Time.deltaTime;
+        x += Time.deltaTime * xSpeed;
+        y += Time.deltaTime * ySpeed;
+        z += Time.deltaTime * zSpeed;
+        this.transform.rotation = Quaternion.Euler(x,y,z);
+    }
+}
